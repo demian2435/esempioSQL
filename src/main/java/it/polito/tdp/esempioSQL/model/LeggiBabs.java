5753c62f9907657f70d930513421d15b4aa9ee12
demian2435 <demian2435@gmail.com>
2020-03-27 18:14:06 +0100

    CreateStatement -> PrepareStatement

--- src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -2,47 +2,51 @@ package it.polito.tdp.esempioSQL.model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class LeggiBabs {
-	
+
 	public void run() {
-		
+
 		// 1 - DEFINIRE L'URL DELLA CONNESSIONE
 		String jdbcURL = "jdbc:mysql://localhost/babs?user=root&password=root";
 		System.out.println("* URL creato");
-		
+
 		try {
 			// 2 - STABILSCO LA ONNESSIONE
 			Connection conn = DriverManager.getConnection(jdbcURL);
 			System.out.println("* Connessione stabilita con il Database");
-			
+
 			// 3 - CREO UNO "STATEMENT OBJECT"
-			Statement st = conn.createStatement();
-			System.out.println("* Creato 'Statement Object'");
-			String sql = "SELECT NAME FROM station";
+			String sql = "SELECT NAME FROM station WHERE landmark = ?";
 			System.out.println("* Creata stringa contenente Query");
-			
-			// 4 - ESEGUI QUERY
-			ResultSet rs = st.executeQuery(sql);
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, "Palo Alto");
+			System.out.println("* Preparo lo 'Statement Object'");
+
+			// 4 - ESEGUO LA QUERY
+			ResultSet rs = st.executeQuery();
 			System.out.println("* Eseguo la Query");
-			
-			// 5 - PROCESSA IL RISULTATO
+
+			// 5 - PROCESSO IL RISULTATO
 			System.out.println("* Processo risultato INIZIO");
-			while(rs.next()) {
+			while (rs.next()) {
 				String nomeStazione = rs.getString("name");
-				System.out.print(nomeStazione + ", ");
+				if (rs.isLast()) {
+					System.out.println(nomeStazione);
+				} else {
+					System.out.print(nomeStazione + ", ");
+				}
 			}
 			st.close();
-			System.out.println("\n* Processo risultato FINE");
+			System.out.println("* Processo risultato FINE");
 
-			// 6 - CHIUDI LA CONNESSIONE
+			// 6 - CHIUDO LA CONNESSIONE
 			conn.close();
 			System.out.println("* Connessione terminata");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
